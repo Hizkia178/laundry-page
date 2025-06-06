f3c40a8f4e29ef961d0ea19b398f6e08c3151051
Hizkia Siahaan <siahaanhizkia548@gmail.com>
2025-06-06 13:47:37 +0700

    none

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Service from "./components/Service";
@@ -7,7 +8,14 @@ import ScrollToTop from "./components/ScrollToTop";
 import Gallery from "./components/Galery";
 import Testimonials from "./components/Testimoni";
 import Location from "./components/Location";
+import Faq from "./components/Faq";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
 function App() {
+  useEffect(() => {
+    AOS.init({ duration: 1200, once: true });
+  }, []);
   return (
     <>
       <Navbar />
@@ -17,6 +25,7 @@ function App() {
       <Gallery />
       <Testimonials />
       <Location />
+      <Faq />
       <ScrollToTop />
       <Footer />
     </>
